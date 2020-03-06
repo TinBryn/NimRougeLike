2eb4b1142b356890970fe3d7c0112625466057ad
Kieran Griffiths <griffk04@gmail.com>
2020-03-06 20:25:54 +0800

    Added basic code to test the tcod library
    This should create a console and render an '@' character
    in a main loop. It successfully creates the window and wont
    hang when it is closed. Unfortunately the rendering doesn't
    work.

--- src/rougelike.nim
+++ src/rougelike.nim
@@ -1,7 +1,33 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
-
 import libtcod
 
+const
+  screenWidth: cint = 80
+  screenHeight: cint = 50
+  limitFPS: cint = 20
+
+type
+  Tcod = object
+    root: Console
+
 when isMainModule:
-  echo("Hello, World!")
+  ##the main entry point
+  consoleSetCustomFont(
+    fontFile = "resources/arial10x10.png",
+    flags = FONT_LAYOUT_TCOD or FONT_TYPE_GREYSCALE
+  )
+
+  consoleInitRoot(
+    w = screenWidth,
+    h = screenHeight,
+    title = "Nim/libtcod tutorial")
+  
+  var tcod = Tcod(root: consoleNew(screenWidth, screenHeight))
+
+  sysSetFps(limitFPS)
+
+  while not consoleIsWindowClosed():
+    tcod.root.consoleSetDefaultForeground(WHITE)
+    tcod.root.consoleClear()
+    tcod.root.consolePutChar(1, 1, '@', BKGND_NONE)
+    consoleFlush()
+    discard consoleWaitForKeypress(false)

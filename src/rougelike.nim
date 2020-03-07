100a598c51906a600d341e505eac32cf82d3ecb0
Kieran Griffiths <griffk04@gmail.com>
2020-03-07 14:08:15 +0800

    Added keyboard support.
    The state is contained in the Game type which currently only
    contains a player, which only has 2D position. The player is
    controlled via the arrow keys, fullscreen is toggled by alt+enter
    and the game is exited with escape. The game can also still be
    exited by closing the window.

--- src/rougelike.nim
+++ src/rougelike.nim
@@ -2,6 +2,8 @@ import libtcod
 
 proc clear(con: Console) = consoleClear(con)
 proc putChar(con: Console, w, h: cint, c: char, flag: BkGndFlag) = consolePutChar(con, w, h, c, flag)
+proc alt(key: Key): bool = key.lalt or key.ralt
+proc consoleToggleFullscreen() = consoleSetFullscreen(not consoleIsFullscreen())
 
 const
   width: cint = 80
@@ -9,7 +11,38 @@ const
   fpsLimit: cint = 60
   root: Console = nil
 
-proc init() =
+type
+  Player = object
+    x, y: cint
+  Game = object
+    player: Player
+
+proc move(player: var Player, x, y: cint) =
+  player.x.inc x
+  player.y.inc y
+
+proc handle_keys(player: var Player): bool =
+  ## updates the player coordinates and returns if the game should quit
+  let key: Key = consoleWaitForKeypress(true)
+  case key.vk:
+  of K_ENTER:
+    if key.alt: consoleToggleFullscreen()
+  of K_ESCAPE: return true
+  of K_UP: player.move(0, -1)
+  of K_DOWN: player.move(0, 1)
+  of K_LEFT: player.move(-1, 0)
+  of K_RIGHT: player.move(1, 0)
+  else:
+    ## default
+  false
+
+proc render(game: var Game) =
+  ## draws the current state to the console
+  root.clear()
+  root.putChar(game.player.x, game.player.y, '@', BKGND_NONE)
+  consoleFlush()
+
+proc init(): Game =
   ## sets up the main console
   consoleSetCustomFont(
     fontFile = "resources/arial10x10.png",
@@ -17,17 +50,17 @@ proc init() =
   )
   consoleInitRoot(width, height, "Nim/rougelike")
   sysSetFps(fpsLimit)
-
-proc mainLoop() =
   root.consoleSetDefaultForeground(WHITE)
+  Game(player: Player(x: width div 2, y: height div 2))
+
+proc mainLoop(game: var Game) =
   while not consoleIsWindowClosed():
-    root.clear()
-    root.putChar(width div 2, height div 2, '@', BKGND_NONE)
-    consoleFlush()
-    discard consoleWaitForKeypress(false)
+    render(game)
+    if handle_keys(game.player):
+      break
 
 when isMainModule:
   ## the main entry point
-  init()
+  var game = init()
 
-  mainLoop()
+  mainLoop(game)
